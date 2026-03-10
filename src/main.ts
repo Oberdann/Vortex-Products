27fc3956e09ab2b06f356ea3fd13bb1150d3c625
Oberdann <oberdandeveloper@gmail.com>
2026-03-10 18:28:07 -0300

    inital commit

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Vortex API - Products')
+    .setDescription('Microsserviço para produtos do ecommerce Vortex')
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
